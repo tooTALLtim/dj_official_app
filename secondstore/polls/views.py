8747386015a48e6720cea06a270e163460df1030
Tim Davis <mitted2@gmail.com>
2023-02-22 14:10:49 -0700

    Set up database, made first migration

--- secondstore/polls/views.py
+++ secondstore/polls/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+    return HttpResponse('Heya cowgirls and cowbows! \
+                         This is the polls index, boy howdy!')
+
+def secret(request):
+    return HttpResponse("Now this is just fun to add, no?")
--- secondstore/secondstore/settings.py
+++ secondstore/secondstore/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'secondstore.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'secondstore',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': 'datab4s3!'
     }
 }
 
@@ -105,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Denver'
 
 USE_I18N = True
 
