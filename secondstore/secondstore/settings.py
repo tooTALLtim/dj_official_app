8d35584d224109953c1e39b2a0827341d2213306
Tim Davis <mitted2@gmail.com>
2023-02-23 21:46:04 -0700

    Added polls/index template

--- secondstore/secondstore/settings.py
+++ secondstore/secondstore/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-w8fh9gisth9cl9dfs$!h3yiwrv+zq8jh#1p#7y1#ya9jc^yla('
+SECRET_KEY = "wouldn't you like to know"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
