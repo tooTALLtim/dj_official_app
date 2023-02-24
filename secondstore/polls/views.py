51e57f5b5c6de4326537277473fe0612b4bbd998
Tim Davis <mitted2@gmail.com>
2023-02-24 12:49:30 -0700

    changed urls to match generic views

--- secondstore/polls/views.py
+++ secondstore/polls/views.py
@@ -13,13 +13,16 @@ class IndexView(generic.ListView):
         """Return the last five published questions"""
         return Question.objects.order_by('-pub_date')[:5]
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question': question})
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/results.html", {"question": question})
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html"
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
