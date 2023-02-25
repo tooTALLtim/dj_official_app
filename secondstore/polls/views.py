e89090cbf7e3a664b64be8b08e9909e6649d55ce
Tim Davis <mitted2@gmail.com>
2023-02-24 17:56:33 -0700

    modified DetailView

--- secondstore/polls/views.py
+++ secondstore/polls/views.py
@@ -20,6 +20,9 @@ class DetailView(generic.DetailView):
     model = Question
     template_name = "polls/detail.html"
 
+    def get_queryset(self):
+        """ Exludes future questions that aren't published yet"""
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 
 class ResultsView(generic.DetailView):
