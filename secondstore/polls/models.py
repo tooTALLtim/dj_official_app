8f367adcb8d16f38aa8c871fff84c5e32238e68c
Tim Davis <mitted2@gmail.com>
2023-02-25 12:23:00 -0700

    finished modifying admin, end of tutorial!

--- secondstore/polls/models.py
+++ secondstore/polls/models.py
@@ -1,4 +1,5 @@
 import datetime
+from django.contrib import admin
 from django.db import models
 from django.utils import timezone
 
@@ -6,6 +7,11 @@ class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
+    @admin.display(
+        boolean=True,
+        ordering='pub_date',
+        description='Was this published in the last day?'
+    )
     def was_published_recently(self):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
