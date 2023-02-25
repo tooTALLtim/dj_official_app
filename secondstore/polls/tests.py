a5df6c9a55c2b11c36b363d2b3108a011a26bce1
Tim Davis <mitted2@gmail.com>
2023-02-24 19:56:50 -0700

    wrote QuestionDetailViewTests

--- secondstore/polls/tests.py
+++ secondstore/polls/tests.py
@@ -16,6 +16,34 @@ def create_question(question_text, days):
     return Question.objects.create(question_text=question_text, pub_date=time)
 
 
+class QuestionDetailViewTests(TestCase):
+    def test_future_question(self):
+        """
+        The detail view of a questions that isn't published yet
+        returns a 404 not found error.
+        """
+        future_question = create_question(question_text="Future question", days=5)
+        url = reverse('polls:detail', args=(future_question.id,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
+
+    def test_past_question(self):
+        """ 
+        The detail view of a question with a pub_date in the past
+        displays in the question's text.
+        """
+        past_question = create_question(question_text="Past question", days=-2)
+        url = reverse('polls:detail', args=(past_question.id,))
+        response = self.client.get(url)
+        self.assertContains(response, past_question.question_text)
+
+
+
+
+
+
+
+
 class QuestionIndexViewTests(TestCase):
     def test_no_questions(self):
         """ If no questions exist, display appropriate error message."""
@@ -60,10 +88,6 @@ class QuestionIndexViewTests(TestCase):
         self.assertQuerysetEqual(
             response.context['latest_question_list'],
             [question2, question1])
-
-
-
-
 
 
 class QuestionModelTests(TestCase):
